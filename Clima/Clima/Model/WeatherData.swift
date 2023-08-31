6097d4b344816e6a0245a9f7bc94214a3aadbe74
Thiago Lopes <thiago.lopes@pyxesportal.com>
2023-08-31 10:54:18 +1000

    Adding weather api rest - Implementing decoder in order to convert the data renponse to swift object

---
+++ Clima/Clima/Model/WeatherData.swift
@@ -0,0 +1,29 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Thiago Américo Lopes on 31/08/23.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+/* Note:
+    May you need some specific attributes that hasn't been mapped here, even though
+    the API is providing, please, just keep mapping as the examples below
+ */
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let description: String
+    let id: Int
+}
--- Clima/Clima/Model/WeatherManager.swift
+++ Clima/Clima/Model/WeatherManager.swift
@@ -21,23 +21,27 @@ struct WeatherManager {
         if let url = URL(string: urlString) {
             let session = URLSession(configuration: .default)
             
-            let task = session.dataTask(with: url, completionHandler: handle(data: response: error:))
-            
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let responseData = data {
+                    self.parseJSON(weatherData: responseData)
+                }
+            }
             task.resume()
         }
     }
     
-    func handle(data: Data?, response:  URLResponse?, error:  Error?) {
-        if error != nil {
-            print(error!)
-            return
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.weather[0].description)
+        } catch {
+            print(error)
         }
-        
-        if let dataReturn = data {
-            let dataReturnString = String(data: dataReturn, encoding: .utf8)
-            print(dataReturnString!)
-        }
-        
-        
     }
 }
