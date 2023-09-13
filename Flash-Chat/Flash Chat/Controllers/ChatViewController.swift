ba8bb5225d78d368eafedf91dafd1e04fc627e42
Thiago Lopes <thiago.lopes@pyxesportal.com>
2023-09-13 10:08:04 +1000

    Adding logOut button on top bar

--- Flash-Chat/Flash Chat/Controllers/ChatViewController.swift
+++ Flash-Chat/Flash Chat/Controllers/ChatViewController.swift
@@ -7,19 +7,34 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
 
 class ChatViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        title = "⚡️FlashChat"
+        navigationItem.hidesBackButton = true
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
     }
     
-
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+        
+        do {
+            try Auth.auth().signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+            print("Error signing out: %@", signOutError)
+        }
+    }
+    
+    
 }
