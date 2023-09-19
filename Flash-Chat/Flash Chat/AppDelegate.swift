020754744ce933cc2df7ace3885abffe75bc5425
Thiago Lopes <thiago.lopes@pyxesportal.com>
2023-09-19 16:54:22 +1000

    Adding the UIKeyboardManager by Swift Package Manager

--- Flash-Chat/Flash Chat/AppDelegate.swift
+++ Flash-Chat/Flash Chat/AppDelegate.swift
@@ -10,6 +10,7 @@ import UIKit
 import FirebaseCore
 import FirebaseFirestore
 import FirebaseAuth
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -21,6 +22,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         FirebaseApp.configure()
         
         let db = Firestore.firestore()
+        
+        print(db)
+        
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
         
         return true
     }
