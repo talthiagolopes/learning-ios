4c01ca911e474056b665f04b5fd74c54f808ebdd
Thiago Lopes <thiago.lopes@pyxesportal.com>
2023-10-10 11:15:11 +1000

    Adding the webview component after click on the news

--- SwiftUI/H1N1/H1N1/NetworkManager.swift
+++ SwiftUI/H1N1/H1N1/NetworkManager.swift
@@ -7,8 +7,11 @@
 
 import Foundation
 
-struct NetworkManager {
+class NetworkManager: ObservableObject {
+    
     let ENDPOINT = "http://hn.algolia.com/api/v1/search?tags=front_page"
+    
+    @Published var posts = [Post]()
     
     func fectData() {
         if let url = URL(string: ENDPOINT) {
@@ -19,6 +22,10 @@ struct NetworkManager {
                     if let dataResponse = data {
                         do {
                             let results = try decoder.decode(PostResponse.self, from: dataResponse)
+                            // Fix: Publishing changes from background threads is not allowed; make sure to publish values from the main thread (via operators like receive(on:)) on model updates.
+                            DispatchQueue.main.async {
+                                self.posts = results.hits
+                            }
                         } catch {
                             print(error)
                         }
---
+++ SwiftUI/H1N1/H1N1/WebView.swift
@@ -0,0 +1,29 @@
+//
+//  WebView.swift
+//  H1N1
+//
+//  Created by Thiago Américo Lopes on 10/10/2023.
+//
+
+import Foundation
+import WebKit
+import SwiftUI
+
+struct WebView: UIViewRepresentable {
+    typealias UIViewType = WKWebView
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeUrlString = urlString {
+            if let url = URL(string: safeUrlString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+}
